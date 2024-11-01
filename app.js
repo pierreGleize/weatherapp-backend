2d1539fe5a2820d73e18a4b9e43b4b8ebe808023
Pierre Gleize <gleizepierre3gmail.com>
2024-11-01 18:55:12 +0100

    ezfhg

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
-require("./models/connection");
+
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
+require("./models/connection");
 
 var indexRouter = require("./routes/index");
 var weatherRouter = require("./routes/weather");
