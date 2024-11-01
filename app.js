246101c9c4b94be080d630b9c764240a7d3f6603
Pierre Gleize <gleizepierre3gmail.com>
2024-11-01 18:05:12 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+require("./models/connection");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var weatherRouter = require("./routes/weather");
+const userRouter = require("./routes/users");
+
+var app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/weather", weatherRouter);
+app.use("/users", userRouter);
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var router = express.Router();
+
+const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
+
+router.post("/signup", async (req, res) => {
+  const emailAllReadyExist = await User.findOne({
+    email: { $regex: new RegExp(req.body.email) },
+  });
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (!checkBody(req.body, ["name", "email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  } else if (emailAllReadyExist) {
+    res.json({ result: false, error: "User already exists" });
+  } else {
+    newUser.save().then((data) => {
+      res.json({ result: true });
+    });
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const foundEmail = await User.findOne({
+    email: { $regex: new RegExp(req.body.email) },
+  });
+  const foundPassword = await User.findOne({
+    password: { $regex: new RegExp(req.body.password) },
+  });
+  if (!checkBody(req.body, ["email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  } else if (!foundEmail || !foundPassword) {
+    res.json({ result: false, error: "User not found" });
+  } else {
+    res.json({ result: true });
+  }
+});
+
+module.exports = router;
